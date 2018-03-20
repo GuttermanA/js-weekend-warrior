08bf3158a0a0d1e15c3d390ffdfad82c5a9bad6a
GuttermanA <alexanderf.gutterman@gmail.com>
2018-03-20 17:10:09 -0400

    small updates for card search

--- app/models/card.rb
+++ app/models/card.rb
@@ -7,12 +7,13 @@ class Card < ApplicationRecord
   has_many :colors, through: :card_colors
 
   belongs_to :mtg_set
+
   validates :name, uniqueness: true
   validates :img_url, presence: true
   # joins is inner join
   # includes is left join
   scope :colors, -> (args) { joins(:colors).where('colors.name': args).references(:colors) }
-  scope :wildcard, -> (column, arg) { where("#{column} = %?%", arg)}
+  scope :wildcard, -> (column, arg) { where("#{column} LIKE ?", "%#{arg}%")}
 
   def self.search(card_params)
     name_wildcard_search = "%#{card_params["name"]}%"
@@ -20,4 +21,5 @@ class Card < ApplicationRecord
 
     Card.where("name LIKE ? OR base_type LIKE ?", name_wildcard_search, type_wildcard_search)
   end
+
 end
